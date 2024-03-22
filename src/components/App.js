8bf0fccb02fd9c6318b8d02cd5c199e9a940147d
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-03-22 06:56:44 +0000

    push1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,61 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useState } from "react";
 const App = () => {
+   //to maintain thses things we need array
+   const [inputValue, setInput] = useState("");
+   const [tasks, setTasks] = useState(["coding"]);
+ 
+   const ChangeInput = (eventObj) => {
+     return setInput(eventObj.target.value);
+   };
+   const addTodo = () => {
+     //console.log(inputValue);
+     //when we have duplicate tasks
+     let found = false;
+     tasks.forEach((task) => {
+       if (task === inputValue) {
+         alert("task already existed");
+         found = true;
+         setInput("");
+       }
+     });
+     if (!found) {
+       setTasks([...tasks, inputValue]);
+       setInput("");
+     }
+   };
+    const removeTask=(taskTobeRemoved) =>{
+     let remainingTodoArr=tasks.filter((taskelem) =>{ return taskelem!==taskTobeRemoved})
+     setTasks(remainingTodoArr);
+   }
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    
+      <div style={{ margin: "20px" }}>
+        <h1>To-Do List</h1>
+        <div>
+          <input
+            placeholder="Enter Todo"
+            value={inputValue}
+            onChange={ChangeInput}
+          />
+          <button onClick={addTodo}>Add Todo</button>
+        </div>
+        <ul className="tasks-list">
+          {tasks.map((task) => {
+            return (
+              <>
+                <li className="task">
+                  <span>{task}</span>
+                  <button onClick={() => removeTask(task)}>Delete</button>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+    
   )
 }
 
